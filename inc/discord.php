72b4333927e74186bce7d6954e0424ed13abae2e
Luna Versity <mrgodzbrother@gmail.com>
2016-10-01 18:24:47 -0700

    Create discord.php

---
+++ inc/discord.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* ez-discord api (MIT)
+* By Lunaversity
+* https://github.com/Lunaversitay/ez-discordwidget/
+*/
+
+$discord_url = "https://discordapp.com/api/servers/".$serverid."/widget.json";
+$json = file_get_contents($discord_url);
+$json_table = json_decode($json, true);
+$channeltables = $json_table;
+
+$discord_svname = $json_table['name'];
+$discord_channels = $json_table['channels'];
+$discord_users = $json_table['members'];
+
+?>
